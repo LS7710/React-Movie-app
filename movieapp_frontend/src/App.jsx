984cd5852d32cc9a44369b5b5b2d6c2021968501
LS7710 <luissoto7710@gmail.com>
2025-06-05 10:34:35 -0400

    testing with components

--- movieapp_frontend/src/App.jsx
+++ movieapp_frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
+  const  movieNumber = 1;
+
   return (
     <>
-    <Text/>
-    <Text/>
+    {movieNumber === 1 ? (
+      <MovieCard movie={{title: "Sharknado 99", release_date: "2025"}} />
+    ) : (
+      <MovieCard movie={{title: "Sharknado 100", release_date: "2026"}}  />
+    )}
     </>
-  )
+  );
 }
 
-function Text() {
-  return(
-    <div>
-      <p>Hello world</p>
-    </div>
-  )
-}
 
 export default App
