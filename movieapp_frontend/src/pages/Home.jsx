2e0eb5656bc8a28e7484e3ccdd267e06959a458f
LS7710 <luissoto7710@gmail.com>
2025-06-05 11:51:03 -0400

    added search bar to home

--- movieapp_frontend/src/pages/Home.jsx
+++ movieapp_frontend/src/pages/Home.jsx
@@ -1,20 +1,31 @@
-import MovieCard from "../components/MovieCard"
+import MovieCard from "../components/MovieCard";
 
 function Home() {
-    const movies = [
-        {id: 1, title: "John Wick", release_date: "2020"},
-        {id: 2, title: "Terminator", release_date: "1990"},
-        {id: 3, title: "Transformers", release_date: "2008"},
-        {id: 4, title: "Avengers", release_date: "2014"},
-    ]
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Terminator", release_date: "1990" },
+    { id: 3, title: "Transformers", release_date: "2008" },
+  ];
 
-    return (
-        <div className = "home">
-            <div className="movies-grid">
-                {movies.map(movies => (
-                    <MovieCard movie={movie} key={movie.id} />
-                ))}
-            </div>
-         </div>
-    )
+  const handleSearch = () => {};
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+        />
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
+export default Home;
