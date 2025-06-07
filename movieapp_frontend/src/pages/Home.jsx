bc00555a5693c2d5a3da4e40711a5e4a535f744f
LS7710 <luissoto7710@gmail.com>
2025-06-07 11:42:33 -0400

    testing with search queries

--- movieapp_frontend/src/pages/Home.jsx
+++ movieapp_frontend/src/pages/Home.jsx
@@ -1,13 +1,20 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     { id: 1, title: "John Wick", release_date: "2020" },
     { id: 2, title: "Terminator", release_date: "1990" },
     { id: 3, title: "Transformers", release_date: "2008" },
   ];
 
-  const handleSearch = () => {};
+  const handleSearch = () => {
+    e.preventDefault()
+    alert(searchQuery)
+    
+  };
 
   return (
     <div className="home">
@@ -16,9 +23,12 @@ function Home() {
           type="text"
           placeholder="Search for movies..."
           className="search-input"
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
         />
+        <button type="submit" className="search-button">Search</button>
       </form>
-
+        
       <div className="movies-grid">
         {movies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
