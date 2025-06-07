15fa651a56294477d92676f8bc824feec9e0a059
LS7710 <luissoto7710@gmail.com>
2025-06-07 12:04:48 -0400

    setting up p

---
+++ movieapp_frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorite() {
+    return <div className="favourite-empty">
+        <h2>You Dont Have Any Favorite Movies Yet</h2>
+        <p>To add movies to your favorites click on the heart icon next to each movie</p>
+    </div>
+}
+
+export default Favorite
