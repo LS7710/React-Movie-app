1d9a073eabce9ea80e870184de4fc561415af57b
LS7710 <luissoto7710@gmail.com>
2025-06-10 11:12:07 -0400

    fixed a name problem in favorites.jsx to work with css

--- movieapp_frontend/src/pages/Favorites.jsx
+++ movieapp_frontend/src/pages/Favorites.jsx
@@ -1,8 +1,10 @@
-function Favorite() {
-    return <div className="favourite-empty">
+import "../css/Favorites.css"
+
+function Favorites() {
+    return <div className="favorites-empty">
         <h2>You Dont Have Any Favorite Movies Yet</h2>
         <p>To add movies to your favorites click on the heart icon next to each movie</p>
     </div>
 }
 
-export default Favorite
+export default Favorites
