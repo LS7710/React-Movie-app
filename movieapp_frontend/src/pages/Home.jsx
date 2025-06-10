5c47a9a1d51b759e9c4a06711532d75fdf6a2031
LS7710 <luissoto7710@gmail.com>
2025-06-10 13:07:15 -0400

    connecting api

--- movieapp_frontend/src/pages/Home.jsx
+++ movieapp_frontend/src/pages/Home.jsx
@@ -1,21 +1,35 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
-import "../css/Home.css"
+import { useState, useEffect } from "react";
+import { searchMovies, getPopularMovies } from "../services/api";
+import "../css/Home.css";
 
 function Home() {
-    const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true)
 
-  const movies = [
-    { id: 1, title: "John Wick", release_date: "2020" },
-    { id: 2, title: "Terminator", release_date: "1990" },
-    { id: 3, title: "Transformers", release_date: "2008" },
-  ];
+  useEffect(() => {
+    const loadPopularMovies = async () => {
+        try {
+            const popularMovies = await getPopularMovies()
+            setMovies(popularMovies)
+        }  catch (err) {
+            console.log(err)
+            setError("Failed to load movies...")
+        }
+        finally {
+            setLoading(false)
+        }
+    } 
+
+    loadPopularMovies() 
+  }, [])
 
   const handleSearch = (e) => {
-    e.preventDefault()
-    alert(searchQuery)
-    setSearchQuery("-----")
-
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("-----");
   };
 
   return (
@@ -28,14 +42,14 @@ function Home() {
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <button type="submit" className="search-button">Search</button>
+        <button type="submit" className="search-button">
+          Search
+        </button>
       </form>
-        
+
       <div className="movies-grid">
         {movies.map((movie) => (
-          (
           <MovieCard movie={movie} key={movie.id} />
-          )
         ))}
       </div>
     </div>
